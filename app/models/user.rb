4928419a1aaa972c6680adf12834ff7269d851dc
robertmackness <robert.mackness.gbr@gmail.com>
2015-09-24 21:33:09 +0100

    Add User tests and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates(:name, presence: true)
+  validates(:name, length: {maximum: 50})
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, on: :create }
+  validates(:email, presence: true)
+  validates(:email, length: {maximum: 255})
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #run before each test
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+    @user2 = User.new(name: "Example User 2", email: "user2@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should be shorter than 50 characters" do 
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+  test "email should be an email" do
+    @user.email = "12345@example,com"
+    assert_not @user.valid?
+  end
+
+  test "email should not be longer than 255 characters" do 
+    @user.email = "a"*255 + "@example.com"
+    assert_not @user.valid?
+  end
+
 end
