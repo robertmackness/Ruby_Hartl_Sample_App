2769e532bad95da269c6ba08aa770d9260bc2663
robertmackness <robert.mackness.gbr@gmail.com>
2015-09-25 15:20:49 +0100

    Add and improve User validation + tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  before_save {self.email = email.downcase}
+  regex = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save {self.email.downcase!}
   validates :name, presence: true,
             length: {maximum: 50}
   validates :email, format: { with: regex, on: :create },
