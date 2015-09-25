e7a4bc238e1b8a2f7d30c3ae529e61254990b39c
robertmackness <robert.mackness.gbr@gmail.com>
2015-09-25 15:11:06 +0100

    Refactor user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
+  regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save {self.email = email.downcase}
   validates :name, presence: true,
             length: {maximum: 50}
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, on: :create },
+  validates :email, format: { with: regex, on: :create },
                     presence: true,
                     length: {maximum: 255},
                     uniqueness: {case_sensitive: false}
