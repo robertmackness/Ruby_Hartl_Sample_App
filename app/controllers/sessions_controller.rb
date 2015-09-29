5a1f4f077531e4e3f8098755e664f10a9f3eebed
robertmackness <robert.mackness.gbr@gmail.com>
2015-09-29 20:23:51 +0100

    Finish login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       # Log the user in and redirect to the user's show page.
       redirect_to user
     else
@@ -17,6 +18,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 
 end
