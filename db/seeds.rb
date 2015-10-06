58e4f97f6860093cb657a8f4a3353917a65b43fb
robertmackness <robert.mackness.gbr@gmail.com>
2015-10-06 14:32:08 +0100

    Fix db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ User.create!(name: "Example User",
   email: "example@railstutorial.org",
   password: "foobar",
   password_confirmation: "foobar",
-  admin: true
-  activated: true
+  admin: true,
+  activated: true,
   activated_at: Time.zone.now)
 
 99.times do |n|
@@ -19,9 +19,8 @@ User.create!(name: "Example User",
   password = "password"
   User.create!( name: name,
                 email: email,
-                password:
-                password,
-                password_confirmation: password
-                activated: true
+                password:              password,
+                password_confirmation: password,
+                activated: true,
                 activated_at: Time.zone.now)
 end
