ac58c953b90f8f8b1e4f362c33bdeae6345db831
robertmackness <robert.mackness.gbr@gmail.com>
2015-10-09 15:58:36 +0100

    Add image upload and resize feature

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,8 +2,22 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
+  # Custom validators use validate not validateS
+  validate :picture_size
 
   # default scope orders by descending create time 
   # the stubby lambda -> or proc just assigns the block to default_scope to be called
   default_scope -> {order(created_at: :desc)}
+
+  #tie PictureUploader to the model
+  mount_uploader :picture, PictureUploader
+
+
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
+
 end
